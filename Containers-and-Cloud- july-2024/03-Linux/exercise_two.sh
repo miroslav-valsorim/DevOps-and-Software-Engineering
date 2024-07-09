dd3aec3fd6f0bae386f31bd30d37e77fbdc54cec
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-07-09 16:02:45 +0300

    linux ex 2

---
+++ Containers-and-Cloud- july-2024/03-Linux/exercise_two.sh
@@ -0,0 +1,15 @@
+$ set | less # env variables
+
+$ set -o # modify the parameters that are driving the environmen
+
+$ set +0 # same information but prepared for re-use
+
+$ set -x
+
+$ pwd
+
+$ ls -al
+
+$ help | less
+
+$ ls --help
