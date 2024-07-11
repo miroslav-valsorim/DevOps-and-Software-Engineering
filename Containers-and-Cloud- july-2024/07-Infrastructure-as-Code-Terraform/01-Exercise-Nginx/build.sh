84eeb379ace1660ea819ae1a2b39a455128369b2
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-07-11 15:46:39 +0300

    IaC Terraform exercise 1

---
+++ Containers-and-Cloud- july-2024/07-Infrastructure-as-Code-Terraform/01-Exercise-Nginx/build.sh
@@ -0,0 +1,13 @@
+docker context ls # shows the docker endpoint that we put in terraform provider host ( npipe:////./pipe/docker_engine )
+
+terraform init
+
+terraform fmt
+
+terraform validate
+
+terraform plan
+
+terraform apply
+
+terraform destroy
