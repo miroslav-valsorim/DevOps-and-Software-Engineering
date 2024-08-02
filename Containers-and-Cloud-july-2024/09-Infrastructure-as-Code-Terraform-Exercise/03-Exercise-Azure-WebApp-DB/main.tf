1fb155914feecb55797c585b8004754d41dbcb88
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-02 15:40:05 +0300

    IaaC ex3 done

---
+++ Containers-and-Cloud-july-2024/09-Infrastructure-as-Code-Terraform-Exercise/03-Exercise-Azure-WebApp-DB/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.114.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {
+  }
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "TaskBoardRG${random_integer.ri.result}"
+  location = "Italy North"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "miro-plan-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "task-board-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.ams.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.amd.name};User ID=${azurerm_mssql_server.ams.administrator_login};Password=${azurerm_mssql_server.ams.administrator_login_password};Trusted_Connection=False;MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      node_version = "16-lts"
+    }
+    always_on = false
+  }
+}
+
+resource "azurerm_mssql_server" "ams" {
+  name                         = "mssqlserver-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+}
+
+resource "azurerm_mssql_database" "amd" {
+  name           = "TaskBoardDB-${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.ams.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.ams.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "aassc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = "https://github.com/miroslav-valsorim/TaskBoardAzureTerraform"
+  branch                 = "main"
+  use_manual_integration = true
+}
---
+++ Containers-and-Cloud-july-2024/09-Infrastructure-as-Code-Terraform-Exercise/03-Exercise-Azure-WebApp-DB/terminal_commands.sh
@@ -0,0 +1,11 @@
+terraform init
+
+terraform fmt
+
+terraform validate
+
+terraform plan
+
+terraform apply
+
+terraform destroy
