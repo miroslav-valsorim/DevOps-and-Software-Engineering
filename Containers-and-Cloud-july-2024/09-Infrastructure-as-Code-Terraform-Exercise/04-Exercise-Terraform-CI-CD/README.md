33173efa7bc0d18869496434676cd072b5f419cf
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-07 12:03:38 +0300

    update terraform CI CD ex4

---
+++ Containers-and-Cloud-july-2024/09-Infrastructure-as-Code-Terraform-Exercise/04-Exercise-Terraform-CI-CD/README.md
@@ -0,0 +1,23 @@
+# Deploying Container Nginx to Azure
+
+![Azure](screenshots/0.png)
+
+![Azure](screenshots/1.png)
+
+![Azure](screenshots/2.png)
+
+![Azure](screenshots/3.png)
+
+![Azure](screenshots/4.png)
+
+![Azure](screenshots/5.png)
+
+![Azure](screenshots/6.png)
+
+![Azure](screenshots/7.png)
+
+![Azure](screenshots/8.png)
+
+![Azure](screenshots/9.png)
+
+![Azure](screenshots/10.png)
--- Containers-and-Cloud-july-2024/09-Infrastructure-as-Code-Terraform-Exercise/04-Exercise-Terraform-CI-CD/terminal-commands.sh
+++ Containers-and-Cloud-july-2024/09-Infrastructure-as-Code-Terraform-Exercise/04-Exercise-Terraform-CI-CD/terminal-commands.sh
@@ -6,11 +6,11 @@ az ad sp create-for-rbac --name "Azure-Terraform-Miro-SP" --role contributor --s
 
 # !!! Steps below are executed for the purpose of saving the tfstate file in the cloud !!!
 # create new resource grp for the storage (hard disc)
-az group create --name StorageRG --location westeurope
+az group create --name StorageRG --location northeurope
 
 # create storage account
-az storage account create --name taskboardstorage --resource-group StorageRG --location westeurope --sku Standard_LRS --kind StorageV2
+az storage account create --name taskboardstoragemhh --resource-group StorageRG --location northeurope --sku Standard_LRS --kind StorageV2
 
 # think of the container as a folder that keeps different files
 # !!! add backend 'azurerm' in the terraform main.tf file 
-az storage container create -n taskboardcontainer --account-name yrtaskboardstorage
+az storage container create -n taskboardcontainer --account-name taskboardstoragemhh
