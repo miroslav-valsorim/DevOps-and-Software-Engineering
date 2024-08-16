4977dd9778c662c396d0a2caa03e980a43e14605
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-08-16 13:57:39 +0300

    exam prep-2 task-2 done

---
+++ Containers-and-Cloud-july-2024/13-Exam-Prep-2/Task-2/README.md
@@ -0,0 +1,5 @@
+# Exam prep 2 - task 2
+
+![task2](screenshots/1.png)
+
+
---
+++ Containers-and-Cloud-july-2024/13-Exam-Prep-2/Task-2/commands.sh
@@ -0,0 +1,9 @@
+terraform init
+
+terraform fmt
+
+terraform validate
+
+terraform plan -var-file="values.tfvars"
+
+terraform apply -var-file="values.tfvars"
